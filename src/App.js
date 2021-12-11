f926ac128efe7b3cc6fb6676c6f9ec3b7f1582d0
fierodev <andriybigyn31@gmail.com>
2021-12-11 17:40:16 +0200

    Test react

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import   {Route, Switch} from 'react-router-dom'
+import AllMeetupsPage from './pages/AllMeetups'
+import NewMeetupPage from './pages/NewMeetup';
+import Favorites from './pages/Favorites';
+import MainNavigation from './layout/MainNavigation';
+
+function App(){
+  return (
+    <div>
+      < MainNavigation />
+      <Switch>
+      <Route path="/" exact>
+        <AllMeetupsPage/>
+      </Route>
+      <Route path="/new-meetup">
+        <NewMeetupPage/>
+      </Route>
+      <Route path="/favorites">
+        <Favorites/>
+      </Route>
+      </Switch>
+    </div>
+  )
+}
+
+export default App; 
